b793ba18c30b3ff6e435e95dfb735146b52a7aa8
Jean <beralb@protonmail.com>
2022-03-02 10:02:57 -0300

    5th commit after part-67

--- zfiles/views_max.py
+++ zfiles/views_max.py
@@ -1,12 +1,9 @@
-from datetime import date
-from django.shortcuts import render
-
-all_posts = [
+posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
         "author": "Maximilian",
-        "date": date(2022, 1, 21),
+        "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
         "content": """
@@ -27,7 +24,7 @@ all_posts = [
         "slug": "programming-is-fun",
         "image": "coding.jpg",
         "author": "Maximilian",
-        "date": date(2022, 2, 10),
+        "date": date(2022, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
         "content": """
@@ -48,7 +45,7 @@ all_posts = [
         "slug": "into-the-woods",
         "image": "woods.jpg",
         "author": "Maximilian",
-        "date": date(2022, 2, 25),
+        "date": date(2020, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
         "content": """
@@ -66,26 +63,3 @@ all_posts = [
         """
     }
 ]
-
-def get_date(post):
-    return post['date']
-
-# Create your views here.
-
-def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
-
-def posts(request):
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
-
-def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug) 
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post 
-    })
